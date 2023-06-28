d1d3cc5fdd9ce36cbff58814357d384c0fb676c7
Jaival Patel <pateljaival05@gmail.com>
2023-06-28 15:45:54 -0400

    posting to db

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
-const db = require('./queries')
+const bodyParser = require('body-parser')
 const app = express()
+const PORT = 3001
+const db = require('./queries')
 
-const PORT = 3000;
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 
 app.get('/', db.getStores)
 app.get('/items', db.getItems)
 app.get('/customers', db.getCustomers)
+app.post('/customer/signin/:username/:password', db.signIn)
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`)
--- queries.js
+++ queries.js
@@ -42,5 +42,27 @@ const getCustomers = (request, response) => {
     })
 }
 
+var prev_id = 0
 
-module.exports = {getStores, getItems, getCustomers}
+const signIn = (request, response) => {
+    const name = request.params['username']
+    const password = request.params['password']
+    const random_id = prev_id 
+    console.log('Name: ', name)
+    console.log('Password: ', password)
+    pool.query("INSERT INTO customer (id, customer_name, customer_password, items_owned, purchase_date, store_ids) VALUES ($1, $2, $3, ARRAY['*'], ARRAY['*'], ARRAY[0]) ", [random_id, name, password], (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            response.status(200).json(results.rows)
+            prev_id += 1
+        }
+    })
+
+    
+
+}
+   
+
+
+module.exports = {getStores, getItems, getCustomers, signIn}
