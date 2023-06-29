c4ed7017a2a689366b562a7d2b9a6515251b0122
Jaival Patel <pateljaival05@gmail.com>
2023-06-28 22:39:48 -0400

    ejs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
+const bcrypt = require('bcrypt')
 const db = require('./queries')
 
 
@@ -18,6 +18,9 @@ app.use(
 )
 
 // get requests 
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 app.get('/stores', db.getStores)
 
 
